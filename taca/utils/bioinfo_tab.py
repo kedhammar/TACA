84e47531a108a78efac01d131f6a5470c5b17c14
Denis Moreno <denis.moreno@scilifelab.se>
2015-07-09 10:28:05 +0200

    senthil's fixes

--- taca/utils/bioinfo_tab.py
+++ taca/utils/bioinfo_tab.py
@@ -21,14 +21,12 @@ def merge(d1, d2):
     """ Will merge dictionary d2 into dictionary d1.
     On the case of finding the same key, the one in d1 will be used.
     :param d1: Dictionary object
-    :param s2: Dictionary object
+    :param d2: Dictionary object
     """
     for key in d2:
         if key in d1:
             if isinstance(d1[key], dict) and isinstance(d2[key], dict):
                 merge(d1[key], d2[key])
-            elif d1[key] == d2[key]:
-                pass # same leaf value
         else:
             d1[key] = d2[key]
     return d1
