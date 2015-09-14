7d5b283618d7fc4bf58203f8a9eb712662307f96
Kate Stepanova <ekaterina.stepanova@scilifelab.se>
2015-09-14 11:45:38 +0200

    Server status

--- taca/server_status/server_status.py
+++ taca/server_status/server_status.py
@@ -1,4 +1,5 @@
-<<<<<<< HEAD
+
+
 import subprocess
 import sys
 
@@ -6,43 +7,43 @@ import json
 import gspread
 from oauth2client.client import SignedJwtAssertionCredentials as GCredentials
 import os
+import logging
 
-
-# for access to server without entering the password
-# public key must be generated on client 
-# and copied to ~/.ssh/authorized_keys of the server 
-SERVERS = ["milou.uppmax.uu.se", "nestor.uppmax.uu.se"]
-USER = "ekat"
-# df -h /home - shows information about home directory as a table
-# awk - takes the prelast column of the table (how many percent) 
-# sed - takes the second line of the output 
-COMMAND = """df -h /home | awk '{print $(NF-1)}' | sed -n 2p"""
-
-# credentials file, must be configure on console.developers.google.com
-G_CREDENTIALS = os.path.join(os.getcwd(), 'gdocs_credentials.json')
-# name of the google doc
-G_SHEET = 'COPY'
-# whatever default value
-G_SCOPE = ['https://spreadsheets.google.com/feeds']
-# to store address of the cell for each server
-G_SHEET_MAP = {	'milou': 'G22',
-				'nestor': 'E22'}
+from config import SERVERS, USER, G_CREDENTIALS, G_SCOPE, G_SHEET, G_SHEET_MAP
 
 def get_disk_space():
 	result = {}
-	for server in SERVERS:
-		# connect via ssh to server and execute the command
-		ssh = subprocess.Popen(['ssh', '-t', '%s@%s' %(USER, server), COMMAND],
-			stdout = subprocess.PIPE,
-			stderr = subprocess.PIPE)
+	for server_url in SERVERS.keys():
+		# get server name, e.g.: milou.uppmax.uu.se -> milou
+		server_name = server_url.split('.')[0]
+		# insert path in command
+		command = SERVERS[server_url]
+
+		logging.debug(server_url)
+		logging.debug(command)
+		print server_url
+		print command
+
+		if server_url == "localhost":
+			ssh = None
+			for subcommand in command.split('|'):
+				logging.debug(subcommand)
+				stdin = ssh.stdout if ssh is not None else subprocess.PIPE
+				ssh = subprocess.Popen(subcommand, stdin=stdin)
+		else:
+			# connect via ssh to server and execute the command
+			ssh = subprocess.Popen(['ssh', '-t', '%s@%s' %(USER, server_url), command],
+				stdout = subprocess.PIPE,
+				stderr = subprocess.PIPE)
 		# recieve how many percent of space left
 		output = ssh.stdout.read()
-		# get server name, e.g.: milou.uppmax.uu.se -> milou
-		server_name = server.split('.')[0] 
+		logging.debug(output)
+		print output
+
 		# from str to int
-		used_space = int(output.replace('%', ''))
+		used_space = int(output.strip().replace('%', ''))
 		available_space = 100 - used_space
-		result[server_name] = str(available_space) + '%' 
+		result[server_name] = str(available_space) + '%'
 	return result
 
 
@@ -61,7 +62,6 @@ def update_google_docs(data):
 
 	# update cell
 	for key in data:
-		cell = G_SHEET_MAP[key] # key = server name
-		value = data[key]		# value = available space
+		cell = G_SHEET_MAP.get(key) # key = server name
+		value = data.get(key)		# value = available space
 		worksheet.update_acell(cell, value)
-	print 'Document has been succesfully updated'
