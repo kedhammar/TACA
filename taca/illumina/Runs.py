7894946b06105cba5d4b4c3d25f454ae737a1f0e
senthil10 <senthilkumar.panneerselvam@scilifelab.se>
2015-10-22 15:59:54 +0200

    add bcl2fastq info in parser object

--- taca/illumina/Runs.py
+++ taca/illumina/Runs.py
@@ -38,7 +38,8 @@ class Run(object):
         self.flowcell_id = m.group(4)
         self.CONFIG      = configuration
         self._set_demux_folder(configuration)
-        self.runParserObj = RunParser(self.run_dir, self.CONFIG)
+        self._get_run_parser_obj(configuration) #get parser object to update DB
+        
 
 
     def demultiplex_run():
@@ -66,6 +67,12 @@ class Run(object):
             return self.sequencer_type
         else:
             raise RuntimeError("sequencer_type not yet available!!")
+    
+    
+    def _get_run_parser_obj(self, configuration):
+        self.runParserObj = RunParser(self.run_dir)
+        if self.runParserObj.obj:
+            self.runParserObj.obj['DemultiplexConfig'] = {'Setup': {'Software': configuration.get('bcl2fastq',{})}}
 
 
     def _set_demux_folder(self, configuration):
