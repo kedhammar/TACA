5cc4af9b8e4d76f6ed540d11caac0a67f329230d
jfnavarro <jc.fernandez.navarro@gmail.com>
2016-02-29 13:43:37 +0100

    Making the unit-tests compatible with travis

--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ setup(name='taca',
     scripts=glob.glob('scripts/*.py'),
     include_package_data=True,
     zip_safe=False,
-    test_suite = 'tests',
     entry_points={
         'console_scripts': ['taca = taca.cli:cli'],
         'taca.subcommands': [
