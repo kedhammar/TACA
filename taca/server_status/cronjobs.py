d8aef5180217fd26d7840c953973676721a33f4f
Kate Stepanova <ekaterina.stepanova@scilifelab.se>
2016-08-31 18:23:51 +0200

    Server status: cronjobs - list of users in the config file

--- taca/server_status/cronjobs.py
+++ taca/server_status/cronjobs.py
@@ -9,24 +9,40 @@ import couchdb
 
 from taca.utils.config import CONFIG
 
+def _parse_crontab():
+    result = {}
+    list_of_users = CONFIG.get('server_status', {}).get('crontab_users', [])
+    if not list_of_users:
+        logging.warning('CONFIG.server_status does not have any crontab_users')
+        current_user = getpass.getuser()
+        list_of_users = [current_user]
+        logging.warning('Using default user: {}'.format(current_user))
+    for user in list_of_users:
+        logging.info('Getting list of cronjobs for user: {}'.format(user))
+        try:
+            crontab = CronTab(user=user)
+        except Exception, e:
+            logging.error('Cannot get a crontab for user: {}'.format(user))
+            logging.error(e.message)
+        else:
+            result[user] = []
+            for job in crontab.crons:
+                result[user].append({'Command': job.command,
+                               'Comment': job.comment,
+                               'Enabled': job.enabled,
+                               'Minute': str(job.minutes),
+                               'Hour': str(job.hours),
+                               'Day of month' : str(job.month),
+                               'Month': str(job.month),
+                               'Day of week': str(job.day)})
+    return result
+
+
 def update_cronjob_db():
-    logging.info('Getting list of cronjobs')
-    user = getpass.getuser()
-    # todo: list of users in config file
-    crontab = CronTab(user=user)
     server = platform.node().split('.')[0]
     timestamp = datetime.datetime.now()
-    result = []
     # parse results
-    for job in crontab.crons:
-        result.append({'Command': job.command,
-                       'Comment': job.comment,
-                       'Enabled': job.enabled,
-                       'Minute': str(job.minutes),
-                       'Hour': str(job.hours),
-                       'Day of month' : str(job.month),
-                       'Month': str(job.month),
-                       'Day of week': str(job.day)})
+    result = _parse_crontab()
     # connect to db
     url = "http://{username}:{password}@{url}:{port}".format(
             url=CONFIG.get('statusdb', {}).get('url'),
@@ -56,7 +72,7 @@ def update_cronjob_db():
         for row in view[server]:
             logging.info('Updating the document')
             doc = crontab_db.get(row.value)
-            doc['users'][user] = result
+            doc['users'] = result
             doc['Last updated'] = str(timestamp)
         if doc:
             try:
