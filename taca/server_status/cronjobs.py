033bf3566d4360572d459465a035fb9f9c2fccef
Kate Stepanova <ekaterina.stepanova@scilifelab.se>
2016-09-01 10:44:07 +0200

    Server status: cronjobs - remove list of users from config

--- taca/server_status/cronjobs.py
+++ taca/server_status/cronjobs.py
@@ -11,30 +11,24 @@ from taca.utils.config import CONFIG
 
 def _parse_crontab():
     result = {}
-    list_of_users = CONFIG.get('server_status', {}).get('crontab_users', [])
-    if not list_of_users:
-        logging.warning('CONFIG.server_status does not have any crontab_users')
-        current_user = getpass.getuser()
-        list_of_users = [current_user]
-        logging.warning('Using default user: {}'.format(current_user))
-    for user in list_of_users:
-        logging.info('Getting list of cronjobs for user: {}'.format(user))
-        try:
-            crontab = CronTab(user=user)
-        except Exception, e:
-            logging.error('Cannot get a crontab for user: {}'.format(user))
-            logging.error(e.message)
-        else:
-            result[user] = []
-            for job in crontab.crons:
-                result[user].append({'Command': job.command,
-                               'Comment': job.comment,
-                               'Enabled': job.enabled,
-                               'Minute': str(job.minutes),
-                               'Hour': str(job.hours),
-                               'Day of month' : str(job.month),
-                               'Month': str(job.month),
-                               'Day of week': str(job.day)})
+    user = getpass.getuser()
+    logging.info('Getting crontab for user {}'.format(user))
+    try:
+        crontab = CronTab(user=user)
+    except Exception, e:
+        logging.error('Cannot get a crontab for user: {}'.format(user))
+        logging.error(e.message)
+    else:
+        result[user] = []
+        for job in crontab.crons:
+            result[user].append({'Command': job.command,
+                           'Comment': job.comment,
+                           'Enabled': job.enabled,
+                           'Minute': str(job.minutes),
+                           'Hour': str(job.hours),
+                           'Day of month' : str(job.month),
+                           'Month': str(job.month),
+                           'Day of week': str(job.day)})
     return result
 
 
@@ -64,7 +58,7 @@ def update_cronjob_db():
         if not view[server].rows:
             logging.info('Creating a document')
             doc = {
-                'users': {user: result},
+                'users': {user: cronjobs for user, cronjob in result.items()},
                 'Last updated': str(timestamp),
                 'server': server,
             }
@@ -72,7 +66,7 @@ def update_cronjob_db():
         for row in view[server]:
             logging.info('Updating the document')
             doc = crontab_db.get(row.value)
-            doc['users'] = result
+            doc['users'].update(result)
             doc['Last updated'] = str(timestamp)
         if doc:
             try:
