47dc9407ed8c052947ba4132db588c1afa70572f
Rickard Hammarén <rickard.hammaren@scilifelab.se>
2017-06-07 12:28:27 +0200

    Fixed the logic regarding symlinking results

--- taca/illumina/HiSeqX_Runs.py
+++ taca/illumina/HiSeqX_Runs.py
@@ -151,6 +151,9 @@ class HiSeqX_Run(Run):
             logger.error("Could not open Demultiplexing directory")
             return
         if complex_run:
+            if not os.path.exists("Demultiplexing"):
+                os.makedirs("Demultiplexing")  ##Making it if it was removed, only useful while debugging
+
             ## We need to merge the Stats.json files and the Demltiplexing directories 
             stats_list=[] #List with the dicts of the two 
             dir_num=0
@@ -162,8 +165,6 @@ class HiSeqX_Run(Run):
                 dir_num +=1
                 os.chdir(self.run_dir)
             #Update the info in Demux_0 (Normal) with the info in Demux_1 (10X)
-            import pdb 
-            pdb.set_trace()
             stats_list[0]['ReadInfosForLanes'].extend(stats_list[1]['ReadInfosForLanes'])
             stats_list[0]['ConversionResults'].extend(stats_list[1]['ConversionResults'])
             stats_list[0]['UnknownBarcodes'].extend(stats_list[1]['UnknownBarcodes'])
@@ -178,11 +179,27 @@ class HiSeqX_Run(Run):
                 if 'Reports' in file_name  or 'Stats' in file_name:        ## The reports are specific to each run and would overwrite each other.            
                     continue 
                 if (os.path.isdir(file_name)) or os.path.isfile(file_name):
-                    os.symlink(os.path.join(dest, os.path.basename(file_name)))
-            
+                    if  os.path.basename(os.path.dirname(file_name)).startswith("Demultiplexing"):
+                        try:
+                            os.symlink(file_name, os.path.join(dest, os.path.basename(file_name)))
+                        except Exception as e:
+                             if e.errno == 17:
+                                 continue  #Don't want an error is file exists, otherwise this will fire for all undetrmined files every time
+                             else:
+                                 logger.info("While trying to create a symlink for {}, TACA encountered error: '{}'".format(file_name,e))
+                    elif os.path.isfile(file_name):   
+                         ## add the subdir of the file to the new path name. i.e Stats to Stats.json. 
+                         try:
+                            os.symlink(file_name, os.path.join(os.path.join(dest, os.path.basename(os.path.normpath(os.path.dirname(file_name))), os.path.basename(file_name))))
+                         except Exception as e:
+                             if e.errno == 17:
+                                 continue  #Don't want an error is file exists, otherwise this will fire for all undetrmined files every time
+                             else:
+                                     logger.info("While trying to create a symlink for {}, TACA encountered error: '{}'".format(file_name,e))
             Statsdir = (os.path.join(dest, "Stats"))
-            os.mkdir(Statsdir)
-            with open (os.path.join(Statsdir, "Stats.json"), w) as json_out:
+            if not os.path.isdir(Statsdir):
+                os.mkdir(Statsdir)
+            with open (os.path.join(Statsdir, "Stats.json"), 'w') as json_out:
                  json.dump(stats_list[0], json_out)
         
         else:
