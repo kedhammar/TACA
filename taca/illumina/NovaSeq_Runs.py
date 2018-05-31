a9143e5200d1c44e68383568de98cb2bcb307903
Chuan Wang <chuan.wang@scilifelab.se>
2018-05-31 09:15:38 +0200

    Remove samplesheet section

--- taca/illumina/NovaSeq_Runs.py
+++ taca/illumina/NovaSeq_Runs.py
@@ -30,35 +30,3 @@ class NovaSeq_Run(HiSeqX_Run):
 
     def _set_run_type(self):
         self.run_type = "NGI-RUN"
-
-    def _generate_clean_samplesheet(self, ssparser):
-        """
-        Will generate a 'clean' samplesheet, for bcl2fastq2.19
-        """
-
-        output=""
-        #Header
-        output+="[Header]{}".format(os.linesep)
-        for field in ssparser.header:
-            output+="{},{}".format(field.rstrip(), ssparser.header[field].rstrip())
-            output+=os.linesep
-        #Data
-        output+="[Data]{}".format(os.linesep)
-        datafields=[]
-        for field in ssparser.datafields:
-            datafields.append(field)
-        output+=",".join(datafields)
-        output+=os.linesep
-        #now parse the data section
-        for line in ssparser.data:
-            line_ar=[]
-            for field in datafields:
-                value = line[field]
-                if ssparser.dfield_sid in field :
-                    value = 'Sample_{}'.format(line[ssparser.dfield_sid])
-                line_ar.append(value)
-
-            output+=",".join(line_ar)
-            output+=os.linesep
-
-        return output
