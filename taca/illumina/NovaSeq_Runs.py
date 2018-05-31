59459947885153861114b01491b81b4586742620
Chuan Wang <chuan.wang@scilifelab.se>
2018-05-31 08:52:34 +0200

    Using HiSeqX as template for NovaSeq

--- taca/illumina/NovaSeq_Runs.py
+++ taca/illumina/NovaSeq_Runs.py
@@ -9,7 +9,7 @@ import subprocess
 from datetime import datetime
 from taca.utils.filesystem import chdir, control_fastq_filename
 from taca.illumina.Runs import Run
-from taca.illumina.HiSeq_Runs import HiSeq_Run
+from taca.illumina.HiSeqX_Runs import HiSeqX_Run
 from taca.utils import misc
 from flowcell_parser.classes import RunParametersParser, SampleSheetParser, RunParser, LaneBarcodeParser, DemuxSummaryParser
 
@@ -18,7 +18,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-class NovaSeq_Run(HiSeq_Run):
+class NovaSeq_Run(HiSeqX_Run):
     def __init__(self,  run_dir, samplesheet_folders):
         super(NovaSeq_Run, self).__init__( run_dir, samplesheet_folders)
         self._set_sequencer_type()
@@ -60,8 +60,5 @@ class NovaSeq_Run(HiSeq_Run):
 
             output+=",".join(line_ar)
             output+=os.linesep
-        
+
         return output
-
-
-
