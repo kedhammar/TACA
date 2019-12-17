773c303a1afb8d19910d0870ec6f47ac22a005ed
Sara Sjunnebo <sara.sjunnebo@gmail.com>
2019-12-17 12:49:07 +0100

    Wrote the functions

--- taca/analysis/analysis_nanopore.py
+++ taca/analysis/analysis_nanopore.py
@@ -4,7 +4,12 @@ Nanopore analysis methods for TACA
 import os
 import logging
 import glob
+import csv
+import subprocess
+import shutil
+from datetime import datetime
 from taca.utils.config import CONFIG
+from taca.utils.transfer import RsyncAgent
 
 logger = logging.getLogger(__name__)
 
@@ -13,18 +18,19 @@ def find_runs_to_process():
     found_run_dirs = []
     for data_dir in data_dirs:
         found = [os.path.join(data_dir, run_dir) for run_dir in os.listdir(data_dir)
-                              if os.path.isdir(os.path.join(data_dir, run_dir))]
+                 if os.path.isdir(os.path.join(data_dir, run_dir))
+                 and run_dir != 'nosync']
         for found_dir in found:
-            found_run_dirs.append(found_dir)  # TODO: See if there is a neater way to do this
+            found_run_dirs.append(found_dir)
     return found_run_dirs
 
 def process_run(run_dir):
     logger.info("Processing run: " + run_dir)
-    run_id = os.path.basename(run_dir)
     summary_file = os.path.join(run_dir, "final_summary.txt")
     demux_dir = os.path.join(run_dir, "nanoseq_output")  # TODO: Check actual name of output dir
     sample_sheet = os.path.join(run_dir, "sample_sheet.csv")  # TODO: Check actual name for sample sheet
     analysis_exit_status_file = os.path.join(run_dir, ".exitcode_for_nanoseq")
+
     if os.path.isfile(summary_file) and not os.path.isdir(demux_dir):
         logger.info("Sequencing done for run " + run_dir + ". Attempting to start analysis.")
         if os.path.isfile(sample_sheet):
@@ -32,16 +38,20 @@ def process_run(run_dir):
 #        else if data available in lims:
 #            make sample_sheet
 #            start_analysis_pipeline(run_dir)
-        else:  # Samplesheet missing and info not found in lims. Notify user and skip.
-            logger.warn("Samplesheet not found for run " + run_dir + ". Operator notified. Skipping.")
+        else:
+            logger.warn("Samplesheet not found for run " + run_dir + ". Operator notified. Skipping.") # TODO: Email operator
     elif os.path.isdir(demux_dir) and not os.path.isfile(analysis_exit_status_file):
         logger.info("Analysis has started for run " + run_dir +" but is not yet done. Skipping.")
     elif os.path.isdir(demux_dir) and os.path.isfile(analysis_exit_status_file):
         analysis_successful = check_exit_status(analysis_exit_status_file)
         if analysis_successful:
-            if is_not_transferred(run_id):
+            run_id = os.path.basename(run_dir)
+            transfer_file_name = CONFIG.get('nanopore_analysis').get('transfer').get('transfer_file')
+            transfer_log = os.path.join(os.path.dirname(run_dir), transfer_file_name)
+            if is_not_transferred(run_id, transfer_log):
                 transfer_run(run_dir)
-            else:
+                update_transfer_log(run_id, transfer_log)
+                logger.info("Run "+ run_dir + " has been synced to the analysis cluster.")
                 archive_run(run_dir)
                 logger.info("Run " + run_dir + " is finished and has been archived.")
                 # TODO: email operator
@@ -54,25 +64,57 @@ def process_run(run_dir):
 
 def start_analysis_pipeline(run_dir):
     # start analysis detatched
-    logger.info("Started analysis for run " + run_dir)
+    analysis_command = "nextflow run nf-core/nanoseq -r dev --help ; echo $? > .exitcode_for_taca.txt"  # TODO: Change to actual command
+    try:
+        p_handle = subprocess.Popen(analysis_command, stdout=subprocess.PIPE, shell=True, cwd=run_dir)
+        logger.info("Started analysis for run " + run_dir)
+    except subprocess.CalledProcessError:
+        logger.warn("An error occurred while starting the analysis for run " + run_dir + ". Please check the logfile for info.")
     return
 
 def check_exit_status(status_file):
-    #read file and return True if 0, False if anything else
-    return True
+    # Read pipeline exit status file and return True if 0, False if anything else
+    with open(status_file, 'r') as f:
+        exit_status = f.readline().strip()
+    if exit_status == '0':
+        return True
+    else:
+        return False
 
-def is_not_transferred(run_id):
-    #return True if id not in transfer.tsv, else False
-    return True
+def is_not_transferred(run_id, transfer_log):
+    # Return True if run id not in transfer.tsv, else False
+    with open(transfer_log, 'r') as f:
+        if run_id not in f.read():
+            return True
+        else:
+            return False
 
 def transfer_run(run_dir):
     #rsync dir to irma
     logger.info("Transferring run " + run_dir + " to analysis cluster")
+    destination = CONFIG.get("nanopore_analysis").get("transfer").get("destination")
+    rsync_opts = {"--no-o" : None, "--no-g" : None, "--chmod" : "g+rw", "-r" : None}
+    connection_details = CONFIG.get("nanopore_analysis").get("transfer").get("analysis_server")
+    transfer_object = RsyncAgent(run_dir, dest_path=destination, remote_host=connection_details["host"], remote_user=connection_details["user"], validate=False, opts=rsync_opts)
+    transfer_object.transfer()
+    return
+
+def update_transfer_log(run_id, transfer_log):
+    with open(transfer_log, 'a') as f:
+        tsv_writer = csv.writer(f, delimiter='\t')
+        tsv_writer.writerow([run_id, str(datetime.now())])
     return
 
 def archive_run(run_dir):
     # mv dir to nosync
     logger.info("Archiving run " + run_dir)
+    archive_dir = CONFIG.get("nanopore_analysis").get("finished_dir")
+    archive_path = os.path.join(os.path.dirname(run_dir), archive_dir, os.path.basename(run_dir))
+    try:
+        shutil.move(run_dir, archive_path)
+        logger.info("Successfully archived " + run_dir)
+    except shutil.Error:
+        logger.warn("An error occurred when archiving " + run_dir + ". Please check the logfile for more info.")
     return
 
 def run_preprocessing(run):
