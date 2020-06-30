a38864c170b6ac8ba9e693a2c5ce09d08977a875
Sara Sjunnebo <sara.sjunnebo@gmail.com>
2020-06-30 10:58:19 +0200

    Check for kit in samplesheet name

--- taca/analysis/analysis_nanopore.py
+++ taca/analysis/analysis_nanopore.py
@@ -1,6 +1,4 @@
-"""
-Nanopore analysis methods for TACA
-"""
+"""Nanopore analysis methods for TACA."""
 import os
 import logging
 import glob
@@ -24,8 +22,8 @@ def find_runs_to_process():
                  if os.path.isdir(os.path.join(nanopore_data_dir, top_dir))
                  and top_dir != 'nosync']
     except OSError:
-        logger.warn("There was an issue locating the following directory: " + nanopore_data_dir +
-                    ". Please check that it exists and try again.")
+        logger.warn('There was an issue locating the following directory: ' + nanopore_data_dir +
+                    '. Please check that it exists and try again.')
     # Get the actual location of the run directories in /var/lib/MinKnow/data/USERDETERMINEDNAME/USERDETSAMPLENAME/run
     if found_top_dirs:
         for top_dir in found_top_dirs:
@@ -33,29 +31,29 @@ def find_runs_to_process():
                 for run_dir in os.listdir(os.path.join(top_dir, sample_dir)):
                     found_run_dirs.append(os.path.join(top_dir, sample_dir, run_dir))
     else:
-        logger.warn("Could not find any run directories in " + nanopore_data_dir)
+        logger.warn('Could not find any run directories in ' + nanopore_data_dir)
     return found_run_dirs
 
 def process_run(run_dir):
-    logger.info("Processing run: " + run_dir)
-    summary_file = os.path.join(run_dir, "final_summary.txt")
-    demux_dir = os.path.join(run_dir, "nanoseq_output")
-    sample_sheet_location = glob.glob(run_dir + "/*sample_sheet.csv")
-    sample_sheet = sample_sheet_location[0] if sample_sheet_location else ""
-    analysis_exit_status_file = os.path.join(run_dir, ".exitcode_for_nanoseq")
+    logger.info('Processing run: ' + run_dir)
+    summary_file = os.path.join(run_dir, 'final_summary.txt')
+    demux_dir = os.path.join(run_dir, 'nanoseq_output')
+    sample_sheet_location = glob.glob(run_dir + '/*sample_sheet.csv')
+    sample_sheet = sample_sheet_location[0] if sample_sheet_location else ''
+    analysis_exit_status_file = os.path.join(run_dir, '.exitcode_for_nanoseq')
     email_recipients = CONFIG.get('mail').get('recipients')
     if os.path.isfile(summary_file) and not os.path.isdir(demux_dir):
-        logger.info("Sequencing done for run " + run_dir + ". Attempting to start analysis.")
+        logger.info('Sequencing done for run ' + run_dir + '. Attempting to start analysis.')
         if os.path.isfile(sample_sheet):
             start_analysis_pipeline(run_dir, sample_sheet)
         else:
-            logger.warn("Samplesheet not found for run " + run_dir + ". Operator notified. Skipping.")
-            email_subject = ("Samplesheet missing for run {}".format(os.path.basename(run_dir)))
-            email_message = """The samplesheet for run {run} is missing and the
-            information can't be found in LIMS. Please add the samplesheet to {run}.""".format(run=run_dir)
+            logger.warn('Samplesheet not found for run ' + run_dir + '. Operator notified. Skipping.')
+            email_subject = ('Samplesheet missing for run {}'.format(os.path.basename(run_dir)))
+            email_message = '''The samplesheet for run {run} is missing and the
+            information can't be found in LIMS. Please add the samplesheet to {run}.'''.format(run=run_dir)
             send_mail(email_subject, email_message, email_recipients)
     elif os.path.isdir(demux_dir) and not os.path.isfile(analysis_exit_status_file):
-        logger.info("Analysis has started for run " + run_dir +" but is not yet done. Skipping.")
+        logger.info('Analysis has started for run ' + run_dir +' but is not yet done. Skipping.')
     elif os.path.isdir(demux_dir) and os.path.isfile(analysis_exit_status_file):
         analysis_successful = check_exit_status(analysis_exit_status_file)
         if analysis_successful:
@@ -64,72 +62,73 @@ def process_run(run_dir):
             if is_not_transferred(run_id, transfer_log):
                 if transfer_run(run_dir):
                     update_transfer_log(run_id, transfer_log)
-                    logger.info("Run "+ run_dir + " has been synced to the analysis cluster.")
+                    logger.info('Run '+ run_dir + ' has been synced to the analysis cluster.')
                     archive_run(run_dir)
-                    logger.info("Run " + run_dir + " is finished and has been archived. Notifying operator.")
-                    email_subject = ("Run successfully processed: {}".format(os.path.basename(run_dir)))
-                    email_message = """Run {} has been analysed, transferred and archived
-                    successfully.""".format(run_dir)
+                    logger.info('Run ' + run_dir + ' is finished and has been archived. Notifying operator.')
+                    email_subject = ('Run successfully processed: {}'.format(os.path.basename(run_dir)))
+                    email_message = '''Run {} has been analysed, transferred and archived
+                    successfully.'''.format(run_dir)
                     send_mail(email_subject, email_message, email_recipients)
                 else:
-                    email_subject = ("Run processed with errors: {}".format(os.path.basename(run_dir)))
-                    email_message = """Run {} has been analysed, but an error occurred during
-                    transfer.""".format(run_dir)
+                    email_subject = ('Run processed with errors: {}'.format(os.path.basename(run_dir)))
+                    email_message = '''Run {} has been analysed, but an error occurred during
+                    transfer.'''.format(run_dir)
                     send_mail(email_subject, email_message, email_recipients)
             else:
-                logger.warn("The following run has already been transferred, skipping: " + run_dir)
+                logger.warn('The following run has already been transferred, skipping: ' + run_dir)
         else:
-            logger.warn("Analysis pipeline exited with a non-zero exit status for run " + run_dir + ". Notifying operator.")
-            email_subject = ("Analysis failed for run {}".format(os.path.basename(run_dir)))
-            email_message = """The analysis failed for run {run}.
-            Please review the logfiles in {run}.""".format(run=run_dir)
+            logger.warn('Analysis pipeline exited with a non-zero exit status for run ' + run_dir + '. Notifying operator.')
+            email_subject = ('Analysis failed for run {}'.format(os.path.basename(run_dir)))
+            email_message = '''The analysis failed for run {run}.
+            Please review the logfiles in {run}.'''.format(run=run_dir)
             send_mail(email_subject, email_message, email_recipients)
     else:
-        logger.info("Run " + run_dir + " not finished yet. Skipping.")
+        logger.info('Run ' + run_dir + ' not finished yet. Skipping.')
     return
 
 def start_analysis_pipeline(run_dir, sample_sheet):
     # start analysis detatched
     flowcell_id = get_flowcell_id(run_dir)
+    kit_id = os.path.basename(sample_sheet).split('_')[0]
     if is_multiplexed(sample_sheet):
-        logger.info("Run " + run_dir + " is multiplexed. Starting nanoseq with --barcode_kit option")
-        analysis_command = "nextflow run nf-core/nanoseq --input " + sample_sheet + \
-            " --run_dir " + run_dir + "/fast5/ \
-            --outdir " + run_dir + "/nanoseq_output \
-            --flowcell " + flowcell_id + \
-            " --guppy_gpu \
+        logger.info('Run ' + run_dir + ' is multiplexed. Starting nanoseq with --barcode_kit option')
+        analysis_command = 'nextflow run nf-core/nanoseq --input ' + sample_sheet + \
+            ' --run_dir ' + run_dir + '/fast5/ \
+            --outdir ' + run_dir + '/nanoseq_output \
+            --flowcell ' + flowcell_id + \
+            ' --guppy_gpu \
             --skip_alignment \
-            --kit SQK-LSK109 \
-            --max_cpus 6 \
+            --kit ' + kit_id + \
+            ' --max_cpus 6 \
             --max_memory 20.GB \
             --barcode_kit EXP-NBD114 \
-            -profile singularity; echo $? > .exitcode_for_nanoseq"
+            -profile singularity; echo $? > .exitcode_for_nanoseq'
     else:
-        logger.info("Run " + run_dir + " is not multiplexed. Starting nanoseq without --barcode_kit option")
-        analysis_command = "nextflow run nf-core/nanoseq --input " + sample_sheet + \
-        " --run_dir " + run_dir + "/fast5/ \
-        --outdir " + run_dir + "/nanoseq_output \
-        --flowcell " + flowcell_id + \
-        " --guppy_gpu \
+        logger.info('Run ' + run_dir + ' is not multiplexed. Starting nanoseq without --barcode_kit option')
+        analysis_command = 'nextflow run nf-core/nanoseq --input ' + sample_sheet + \
+        ' --run_dir ' + run_dir + '/fast5/ \
+        --outdir ' + run_dir + '/nanoseq_output \
+        --flowcell ' + flowcell_id + \
+        ' --guppy_gpu \
         --skip_alignment \
-        --kit SQK-LSK109 \
-        --max_cpus 6 \
+        --kit ' + kit_id + \
+        ' --max_cpus 6 \
         --max_memory 20.GB \
-        -profile singularity; echo $? > .exitcode_for_nanoseq"
+        -profile singularity; echo $? > .exitcode_for_nanoseq'
 
     try:
         p_handle = subprocess.Popen(analysis_command, stdout=subprocess.PIPE, shell=True, cwd=run_dir)
-        logger.info("Started analysis for run " + run_dir)
+        logger.info('Started analysis for run ' + run_dir)
     except subprocess.CalledProcessError:
-        logger.warn("An error occurred while starting the analysis for run " + run_dir + ". Please check the logfile for info.")
+        logger.warn('An error occurred while starting the analysis for run ' + run_dir + '. Please check the logfile for info.')
     return
 
 def get_flowcell_id(run_dir):
     # Look for flow_cell_product_code in report.md and return the corresponding value
-    report_file = os.path.join(run_dir, "report.md")
+    report_file = os.path.join(run_dir, 'report.md')
     with open(report_file, 'r') as f:
         for line in f.readlines():
-            if "flow_cell_product_code" in line:
+            if 'flow_cell_product_code' in line:
                 return line.split('"')[3]
 
 def is_multiplexed(sample_sheet):
@@ -137,7 +136,7 @@ def is_multiplexed(sample_sheet):
     # Assumes that a run that has not been multiplexed has the barcode 0
     with open(sample_sheet, 'r') as f:
         for i, line in enumerate(f):
-            if i == 1:
+            if i == 1: # Only need to check first non-header line
                 line_entries = line.split(',')
     if line_entries[2] == '0':
         return False
@@ -164,15 +163,15 @@ def is_not_transferred(run_id, transfer_log):
 
 def transfer_run(run_dir):
     #rsync dir to irma
-    logger.info("Transferring run " + run_dir + " to analysis cluster")
-    destination = CONFIG.get("nanopore_analysis").get("transfer").get("destination")
-    rsync_opts = {"-Lav": None, "--no-o" : None, "--no-g" : None, "--chmod" : "g+rw", "-r" : None, "--exclude" : "work"}
-    connection_details = CONFIG.get("nanopore_analysis").get("transfer").get("analysis_server")
-    transfer_object = RsyncAgent(run_dir, dest_path=destination, remote_host=connection_details["host"], remote_user=connection_details["user"], validate=False, opts=rsync_opts)
+    logger.info('Transferring run ' + run_dir + ' to analysis cluster')
+    destination = CONFIG.get('nanopore_analysis').get('transfer').get('destination')
+    rsync_opts = {'-Lav': None, '--no-o' : None, '--no-g' : None, '--chmod' : 'g+rw', '-r' : None, '--exclude' : 'work'}
+    connection_details = CONFIG.get('nanopore_analysis').get('transfer').get('analysis_server')
+    transfer_object = RsyncAgent(run_dir, dest_path=destination, remote_host=connection_details['host'], remote_user=connection_details['user'], validate=False, opts=rsync_opts)
     try:
         transfer_object.transfer()
     except RsyncError:
-        logger.warn("An error occurred while transferring " + run_dir + " to the ananlysis server. Please check the logfiles")
+        logger.warn('An error occurred while transferring ' + run_dir + ' to the ananlysis server. Please check the logfiles')
         return False
     return True
 
@@ -182,18 +181,18 @@ def update_transfer_log(run_id, transfer_log):
             tsv_writer = csv.writer(f, delimiter='\t')
             tsv_writer.writerow([run_id, str(datetime.now())])
     except IOError:
-        logger.warn("Could not update the transfer logfile for run " + run_id + ". Please make sure " + transfer_log + " gets updated.")
+        logger.warn('Could not update the transfer logfile for run ' + run_id + '. Please make sure ' + transfer_log + ' gets updated.')
     return
 
 def archive_run(run_dir):
     # mv dir to nosync
-    logger.info("Archiving run " + run_dir)
-    archive_dir = CONFIG.get("nanopore_analysis").get("finished_dir")
+    logger.info('Archiving run ' + run_dir)
+    archive_dir = CONFIG.get('nanopore_analysis').get('finished_dir')
     try:
         shutil.move(run_dir, archive_dir)
-        logger.info("Successfully archived " + run_dir)
+        logger.info('Successfully archived ' + run_dir)
     except shutil.Error:
-        logger.warn("An error occurred when archiving " + run_dir + ". Please check the logfile for more info.")
+        logger.warn('An error occurred when archiving ' + run_dir + '. Please check the logfile for more info.')
     return
 
 def run_preprocessing(run):
