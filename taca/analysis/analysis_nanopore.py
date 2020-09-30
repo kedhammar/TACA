402bc7ee08b9b7458ee549ec0806e7b0958a88df
Sara Sjunnebo <sara.sjunnebo@gmail.com>
2020-09-30 11:59:51 +0200

    Add spaces to make main processing function more readable

--- taca/analysis/analysis_nanopore.py
+++ taca/analysis/analysis_nanopore.py
@@ -41,6 +41,7 @@ def process_run(run_dir, nanoseq_sample_sheet, anglerfish_sample_sheet):
     qc_run = True
     if nanoseq_sample_sheet and not anglerfish_sample_sheet:
         qc_run = False
+
     logger.info('Processing run: {} as a {}'.format(run_dir, 'QC run' if qc_run else 'non-QC run'))
     summary_file = os.path.join(run_dir, 'final_summary.txt')
     nanoseq_dir = os.path.join(run_dir, 'nanoseq_output')
@@ -48,29 +49,37 @@ def process_run(run_dir, nanoseq_sample_sheet, anglerfish_sample_sheet):
     nanoseq_exit_status_file = os.path.join(run_dir, '.exitcode_for_nanoseq')
     anglerfish_exit_status_file = os.path.join(run_dir, '.exitcode_for_anglerfish')
     email_recipients = CONFIG.get('mail').get('recipients')
+
     if os.path.isfile(summary_file) and not os.path.isdir(nanoseq_dir):
         logger.info('Sequencing done for run {}. Attempting to start analysis.'.format(run_dir))
         if not nanoseq_sample_sheet:
             nanoseq_sample_sheet, anglerfish_sample_sheet = parse_lims_sample_sheet(run_dir)
+
         if os.path.isfile(nanoseq_sample_sheet):
             start_nanoseq(run_dir, nanoseq_sample_sheet)
+
         else:
             logger.warn('Samplesheet not found for run {}. Operator notified. Skipping.'.format(run_dir))
             email_subject = ('Samplesheet missing for run {}'.format(os.path.basename(run_dir)))
             email_message = 'There was an issue locating the samplesheet for run {}.'.format(run_dir)
             send_mail(email_subject, email_message, email_recipients)
+
     elif os.path.isdir(nanoseq_dir) and not os.path.isfile(nanoseq_exit_status_file):
         logger.info('Nanoseq has started for run {} but is not yet done. Skipping.'.format(run_dir))
+
     elif os.path.isdir(nanoseq_dir) and os.path.isfile(nanoseq_exit_status_file):
         nanoseq_successful = check_exit_status(nanoseq_exit_status_file)
         if nanoseq_successful:
             run_id = os.path.basename(run_dir)
             transfer_log = CONFIG.get('nanopore_analysis').get('transfer').get('transfer_file')
+
             if qc_run and not os.path.isdir(anglerfish_dir):
                 logger.info('Nanoseq done for run {}. Attempting to start Anglerfish.'.format(run_id))
                 start_anglerfish(run_dir, anglerfish_sample_sheet, anglerfish_dir)
+
             elif qc_run and not os.path.isfile(anglerfish_exit_status_file):
                 logger.info('Anglerfish has started for run {} but is not yet done. Skipping.'.format(run_id))
+
             elif qc_run and os.path.isfile(anglerfish_exit_status_file):
                 anglerfish_successful = check_exit_status(anglerfish_exit_status_file)
                 if anglerfish_successful:
@@ -80,6 +89,7 @@ def process_run(run_dir, nanoseq_sample_sheet, anglerfish_sample_sheet):
                     email_message = ('Anglerfish has successfully finished for run {}. Please '
                                      'finish the QC step in lims.').format(run_id)
                     send_mail(email_subject, email_message, email_recipients)
+
                     if is_not_transferred(run_id, transfer_log):
                         if transfer_run(run_dir):
                             update_transfer_log(run_id, transfer_log)
@@ -90,6 +100,7 @@ def process_run(run_dir, nanoseq_sample_sheet, anglerfish_sample_sheet):
                             email_message = ('Run {} has been analysed, transferred and archived '
                                              'successfully.').format(run_id)
                             send_mail(email_subject, email_message, email_recipients)
+
                         else:
                             logger.warn('An error occurred during transfer of run {} '
                                         'to Irma. Notifying operator.'.format(run_dir))
@@ -97,9 +108,11 @@ def process_run(run_dir, nanoseq_sample_sheet, anglerfish_sample_sheet):
                             email_message = ('Run {} has been analysed, but an error occurred during '
                                              'transfer.').format(run_id)
                             send_mail(email_subject, email_message, email_recipients)
+
                     else:
                         logger.warn('The following run has already been transferred, '
                                     'skipping: {}'.format(run_dir))
+
                 else:
                     logger.warn('Anglerfish exited with a non-zero exit status for run {}. '
                                 'Notifying operator.'.format(run_dir))
@@ -107,6 +120,7 @@ def process_run(run_dir, nanoseq_sample_sheet, anglerfish_sample_sheet):
                     email_message = ('Anglerfish exited with errors for run {}. Please '
                                      'check the log files and restart.').format(run_id)
                     send_mail(email_subject, email_message, email_recipients)
+
             elif not qc_run:
                 if is_not_transferred(run_id, transfer_log):
                     if transfer_run(run_dir):
@@ -118,6 +132,7 @@ def process_run(run_dir, nanoseq_sample_sheet, anglerfish_sample_sheet):
                         email_message = ('Run {} has been analysed, transferred and archived '
                                          'successfully.').format(run_id)
                         send_mail(email_subject, email_message, email_recipients)
+
                     else:
                         logger.warn('An error occurred during transfer of run {} '
                                     'to Irma. Notifying operator.'.format(run_dir))
@@ -125,17 +140,21 @@ def process_run(run_dir, nanoseq_sample_sheet, anglerfish_sample_sheet):
                         email_message = ('Run {} has been analysed, but an error occurred during '
                                          'transfer.').format(run_id)
                         send_mail(email_subject, email_message, email_recipients)
+
                 else:
                     logger.warn('The following run has already been transferred, '
                                 'skipping: {}'.format(run_id))
+
         else:
             logger.warn('Nanoseq exited with a non-zero exit status for run {}. '
                         'Notifying operator.'.format(run_dir))
             email_subject = ('Analysis failed for run {}'.format(os.path.basename(run_dir)))
             email_message = 'The nanoseq analysis failed for run {}.'.format(run_dir)
             send_mail(email_subject, email_message, email_recipients)
+
     else:
         logger.info('Run {} not finished sequencing yet. Skipping.'.format(run_dir))
+
     return
 
 def parse_lims_sample_sheet(run_dir):
