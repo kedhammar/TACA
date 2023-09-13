417957caada67d37e3ad275faec2cb815d27324e
kedhammar <alfred.kedhammar@scilifelab.se>
2023-09-13 14:22:34 +0200

    quotes

--- taca/nanopore/ONT_run_classes.py
+++ taca/nanopore/ONT_run_classes.py
@@ -52,7 +52,7 @@ class ONT_run(object):
 
         # Get instrument
         # - For PromethION, the positions will be "1A, 2A, ..., 3G".
-        # - For MinION the position will be the instrument ID e.g. MN19414.
+        # - For MinION the position will be the instrument ID e.g. "MN19414".
         self.instrument = "promethion" if len(self.position) == 2 else "minion"
 
         # Get run type
