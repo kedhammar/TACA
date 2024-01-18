c86676a0c6f99d51cee50dbaba641d8c8e9a6946
chuan-wang <chuan.wang@scilifelab.se>
2024-01-18 15:10:29 +0100

    Change run name for NextSeq super init

--- taca/illumina/NextSeq_Runs.py
+++ taca/illumina/NextSeq_Runs.py
@@ -3,7 +3,7 @@ from taca.illumina.Standard_Runs import Standard_Run
 
 class NextSeq_Run(Standard_Run):
     def __init__(self, run_dir, software, configuration):
-        super(Standard_Run, self).__init__( run_dir, software, configuration)
+        super(NextSeq_Run, self).__init__( run_dir, software, configuration)
         self._set_sequencer_type()
         self._set_run_type()
         # NextSeq2000 has a different FC ID pattern that ID contains the first letter for position
