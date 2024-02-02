f44e48cdedb89f832c7bf5800c8c10b4643bd98f
kedhammar <alfred.kedhammar@scilifelab.se>
2024-02-02 18:27:06 +0100

    good progress

--- tests/pytest/test_ONT_run_classes.py
+++ tests/pytest/test_ONT_run_classes.py
@@ -1,7 +1,9 @@
 import os
 import tempfile
+from unittest.mock import patch
 
 import pytest
+import yaml
 
 from taca.nanopore import ONT_run_classes
 
@@ -9,11 +11,64 @@ from taca.nanopore import ONT_run_classes
 # pytest -s --cov=taca.nanopore.ONT_run_classes --cov-report term-missing -vv tests/pytest/test_ONT_run_classes.py
 
 
+def make_test_config(tmp):
+    test_config_yaml_string = f"""nanopore_analysis:
+    run_types:
+        user_run:
+        data_dirs:
+            - {tmp.name}/sequencing/promethion
+            - {tmp.name}/sequencing/minion
+        ignore_dirs:
+            - 'nosync'
+            - 'qc'
+        instruments:
+            promethion:
+                transfer_log: {tmp.name}/log/transfer_promethion.tsv
+                archive_dir: {tmp.name}/sequencing/promethion/nosync
+                metadata_dir: {tmp.name}/ngi-nas-ns/promethion_data
+                destination: {tmp.name}/miarka/promethion/
+            minion:
+                transfer_log: /{tmp.name}/log/transfer_minion.tsv
+                archive_dir: {tmp.name}/sequencing/minion/nosync
+                metadata_dir: {tmp.name}/ngi-nas-ns/minion_data
+                destination: {tmp.name}/miarka/minion/
+        qc_run:
+            data_dirs:
+                - {tmp.name}/ngi_data/sequencing/minion/qc
+        ignore_dirs:
+            - 'nosync'
+        instruments:
+            minion:
+                transfer_log: {tmp.name}/log/transfer_minion_qc.tsv
+                archive_dir: {tmp.name}/sequencing/minion/qc/nosync
+                metadata_dir: {tmp.name}/ngi-nas-ns/minion_data/qc
+                destination: {tmp.name}/miarka/minion/qc
+        anglerfish:
+            anglerfish_samplesheets_dir: /srv/ngi-nas-ns/samplesheets/anglerfish
+            anglerfish_path: ~/miniconda3/envs/anglerfish/bin/anglerfish
+    minknow_reports_dir: {tmp.name}/minknow_reports/
+    analysis_server:
+        host: miarka1.uppmax.uu.se
+        port:
+        user: funk_903
+    rsync_options:
+        '-Lav': None
+        '--chown': ':ngi2016003'
+        '--chmod': 'Dg+s,g+rw'
+        '-r': None
+        '--exclude': ['work']"""
+
+    test_config_yaml = yaml.safe_load(test_config_yaml_string)
+
+    return test_config_yaml
+
+
 @pytest.fixture
 def create_dirs():
     """Create the bottom-level file-tree to be used for all tests:
 
     tmp
+    ├── minknow_reports
     └── sequencing
         ├── promethion_data
         │   └── nosync
@@ -26,10 +81,28 @@ def create_dirs():
     """
     tmp = tempfile.TemporaryDirectory()
 
-    # Create the sequencing data dir structure
+    # CREATE DIR STRUCTURE
+
+    # Sequencing data
     os.makedirs(f"{tmp.name}/sequencing/promethion_data/nosync")
     os.makedirs(f"{tmp.name}/sequencing/minion_data/nosync")
     os.makedirs(f"{tmp.name}/sequencing/minion_data/qc/nosync")
+
+    # Non-sensitive metadata
+    os.makedirs(f"{tmp.name}/ngi-nas-ns/promethion_data")
+    os.makedirs(f"{tmp.name}/ngi-nas-ns/minion_data")
+
+    # Reports for GenStat
+    os.makedirs(f"{tmp.name}/minknow_reports")
+
+    # Logs
+    os.makedirs(f"{tmp.name}/log")
+    open(f"{tmp.name}/log/transfer_promethion.tsv", "w").close()
+    open(f"{tmp.name}/log/transfer_minion.tsv", "w").close
+
+    # Analysis server destination dirs
+    os.makedirs(f"{tmp.name}/miarka/promethion")
+    os.makedirs(f"{tmp.name}/miarka/minion/qc")
 
     yield tmp
 
@@ -63,6 +136,10 @@ def create_run_dir(
 
 def test_ONT_user_run(create_dirs):
     tmp = create_dirs
+
+    # Mock the CONFIG
+    mock_config = patch("taca.utils.config.CONFIG", new=make_test_config(tmp))
+    mock_config.start()
 
     run_path = create_run_dir(tmp)
     run = ONT_run_classes.ONT_user_run(run_path)
