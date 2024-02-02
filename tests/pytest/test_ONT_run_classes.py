bcc91ea5d8ffbcca46d4cac39479685b2d38e75b
kedhammar <alfred.kedhammar@scilifelab.se>
2024-02-02 14:23:14 +0100

    wip

--- tests/pytest/test_ONT_run_classes.py
+++ tests/pytest/test_ONT_run_classes.py
@@ -1,20 +1,16 @@
-import json
 import os
-import re
 import tempfile
-from unittest.mock import Mock, call, mock_open, patch
-from taca.nanopore import ONT_run_classes
 
 import pytest
 
-from taca.nanopore import instrument_transfer
+from taca.nanopore import ONT_run_classes
 
 # To check coverage, use
-# pytest -s --cov=taca.nanopore.instrument_transfer --cov-report term-missing -vv test_instrument_transfer.py
+# pytest -s --cov=taca.nanopore.ONT_run_classes --cov-report term-missing -vv tests/pytest/test_ONT_run_classes.py
 
 
 @pytest.fixture
-def create_dirs() -> tempfile.TemporaryDirectory:
+def create_dirs():
     """Create the bottom-level file-tree to be used for all tests:
 
     tmp
@@ -35,7 +31,9 @@ def create_dirs() -> tempfile.TemporaryDirectory:
     os.makedirs(f"{tmp.name}/sequencing/minion_data/nosync")
     os.makedirs(f"{tmp.name}/sequencing/minion_data/qc/nosync")
 
-    return tmp
+    yield tmp
+
+    tmp.cleanup()
 
 
 def create_run_dir(
@@ -45,14 +43,28 @@ def create_run_dir(
     flowcell_id="TEST12345",
     data_dir=None,
 ):
-    """Create a run directory.
-    --> Return it's path.
+    """Create a run directory according to specifications.
+
+    ..
+    └── {data_dir}
+        └── 20240131_1702_{instrument_position}_{flowcell_id}_randomhash
+
+    Return it's path.
     """
     if not data_dir:
-        data_dir = f"{tmp.name}/sequencing/{instrument}_data/"
+        data_dir = f"{tmp.name}/sequencing/{instrument}_data"
 
     run_name = f"20240131_1702_{instrument_position}_{flowcell_id}_randomhash"
     run_path = f"{data_dir}/{run_name}"
     os.mkdir(run_path)
 
     return run_path
+
+
+def test_ONT_user_run(create_dirs):
+    tmp = create_dirs
+
+    run_path = create_run_dir(tmp)
+    run = ONT_run_classes.ONT_user_run(run_path)
+
+    assert run.run_abspath == run_path
