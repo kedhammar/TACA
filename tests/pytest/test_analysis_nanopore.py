35d33a5b709e5b575618b0ac48956bf52722abc8
kedhammar <alfred.kedhammar@scilifelab.se>
2024-02-06 14:53:06 +0100

    update test

--- tests/pytest/test_analysis_nanopore.py
+++ tests/pytest/test_analysis_nanopore.py
@@ -9,7 +9,7 @@ from test_ONT_run_classes import (
 from taca.analysis import analysis_nanopore
 
 # To check coverage, use
-# pytest -s --cov=taca.analysis.analysis_nanopore --cov-report term-missing -vv tests/pytest/test_analysis_nanopore.py
+# pytest -s --log-cli-level=INFO --cov=taca.analysis.analysis_nanopore --cov-report term-missing -vv tests/pytest/test_analysis_nanopore.py
 
 
 def test_ont_transfer(create_dirs, caplog):
@@ -19,20 +19,28 @@ def test_ont_transfer(create_dirs, caplog):
 
     # Create dir tree from fixture
     tmp = create_dirs
+
     # Create test config
     test_config_yaml = make_test_config(tmp)
 
-    # Set up mocks
-    patch("taca.utils.statusdb.NanoporeRunsConnection").start()
+    # Set up config mocks
     patch("taca.utils.config.CONFIG", new=test_config_yaml).start()
     patch("taca.nanopore.ONT_run_classes.CONFIG", new=test_config_yaml).start()
+
+    # Set up mocks
+    mock_db = patch(
+        "taca.nanopore.ONT_run_classes.NanoporeRunsConnection",
+    ).start()
+    mock_db.return_value.check_run_exists.return_value = False
+    mock_db.return_value.check_run_status.return_value = "ongoing"
+
     mock_error = patch("taca.analysis.analysis_nanopore.send_error_mail").start()
 
     # Reload module to add mocks
     importlib.reload(analysis_nanopore)
 
     # Create run dir
-    create_run_dir(tmp)
+    create_run_dir(tmp, script_files=True, run_finished=True, sync_finished=True)
 
     # Start testing
     analysis_nanopore.ont_transfer(run_abspath=None, qc=False)
