1a4320750e8b13294cdc996658f3f36d3a649db1
Sara Sjunnebo <sara.sjunnebo@gmail.com>
2024-09-10 15:20:00 +0200

    Add status of a run

--- taca/analysis/analysis_element.py
+++ taca/analysis/analysis_element.py
@@ -10,14 +10,6 @@ from taca.utils.config import CONFIG
 logger = logging.getLogger(__name__)
 
 
-def _upload_to_statusdb(run):
-    """Triggers the upload to statusdb.
-
-    :param Run run: the object run
-    """
-    pass
-
-
 def run_preprocessing(given_run):
     """Run demultiplexing in all data directories.
 
@@ -29,20 +21,19 @@ def run_preprocessing(given_run):
 
         :param taca.element.Run run: Run to be processed and transferred
         """
-        # TODO: Fetch statusdb document for run
-        # TODO: Get previous status of run from statusdb document
         sequencing_done = run.check_sequencing_status()
         demultiplexing_status = run.get_demultiplexing_status()
-        if not sequencing_done:
-            # TODO: compare previous status with current status and update statusdb document if different
-            return
-        elif sequencing_done and demultiplexing_status == "not started":
+        if not sequencing_done: # Sequencing ongoing
+            current_run_status = 'sequencing'
+            if run.status_changed(current_run_status):
+                run.update_statusdb(current_run_status) #TODO: what info needs to be gathered and uploaded?
+        elif sequencing_done and demultiplexing_status == "not started": # Sequencing done. Start demux
             if not run.manifest_exists():
+                logger.warn(f"Run manifest is missing for {run.flowcell_id}")
                 #TODO: email operator warning
                 return
             elif run.manifest_exists():
-                # Get sample info from manifest
-                sample_info = run.get_sample_info()
+                sample_info = run.get_sample_info_from_manifest()
                 sample_types = run.get_sample_types(sample_info)
                 if len(sample_types) == 1:
                     run.start_demux()
@@ -51,48 +42,59 @@ def run_preprocessing(given_run):
                         run.make_manifest(sample_info, sample_type)
                         run.start_demux()
                 else:
-                    #TODO: warn that no samples were found in the run manifest
+                    logger.warn(f"No samples were found in the sample manifest for run {run.flowcell_id}.")
+                    #TODO: email operator warning
                     return
-                #TODO: compare previous status with current status and update statusdb document if different
+                current_run_status = "demultiplexing"
+                if run.status_changed(current_run_status):
+                    run.update_statusdb(current_run_status)
         elif sequencing_done and demultiplexing_status == "ongoing":
-            # TODO: compare previous status with current status and update statusdb document if different
+            current_run_status = "demultiplexing"
+            if run.status_changed(current_run_status):
+                run.update_statusdb(current_run_status)
             return
         elif sequencing_done and demultiplexing_status == "finished":
             transfer_file = CONFIG.get('Element').get('Aviti').get('transfer_log')
             if not run.is_transferred(transfer_file) and not run.transfer_ongoing():
                 run.sync_metadata()
                 run.make_transfer_indicator()
-                #TODO: compare previous status with current status and update statusdb document if different
-                    # Also update statusdb with a timestamp of when the transfer started
+                current_run_status = "transferring"
+                if run.status_changed(current_run_status):
+                    run.update_statusdb(current_run_status)
+                    #TODO: Also update statusdb with a timestamp of when the transfer started
                 run.transfer()
                 run.remove_transfer_indicator()
                 run.update_transfer_log(transfer_file)
-                #TODO: update statusdb document
+                current_run_status = "transferred"
+                if run.status_changed(current_run_status):
+                    run.update_statusdb(current_run_status)
                 run.archive()
+                current_run_status = "archived"
+                if run.status_changed(current_run_status):
+                    run.update_statusdb(current_run_status)
             elif not run.is_transferred(transfer_file) and run.transfer_ongoing():
-                #TODO: compare previous status with current status and update statusdb document if different
-                logger.info("Run is being transferred. Skipping.")
+                current_run_status = "transferring"
+                if run.status_changed(current_run_status):
+                    run.update_statusdb(current_run_status)
+                logger.info(f"Run {run.flowcell_id} is being transferred. Skipping.")
                 return
             elif run.is_transferred(transfer_file):
-                #TODO: compare previous status with current status and update statusdb document if different
-                # warn that transferred run has not been archived
-                logger.warn("The run has already been transferred but has not been archived. Please investigate")
+                logger.warn(f"The run {run.flowcell_id} has already been transferred but has not been archived. Please investigate")
+                #TODO: email operator warning
                 return
             else:
-                logger.warn("Unknown transfer status. Please investigate")
+                logger.warn(f"Unknown transfer status of run {run.flowcell_id}. Please investigate")
                 
             
 
     if given_run:
-        run = Aviti_Run(
-            run
-        )  # TODO: Needs to change if more Element machines are aquired in the future
+        run = Aviti_Run(given_run)  # TODO: Needs to change if more Element machines are aquired in the future
         _process(run)
     else:
         data_dirs = CONFIG.get("element_analysis").get(
             "data_dirs"
         )  # TODO: add to config
-        for data_dir in data_dirs:
+        for data_dir in data_dirs: #TODO: make sure to look in both side A and B
             # Run folder looks like DATE_*_*_*, the last section is the FC name.
             runs = glob.glob(
                 os.path.join(data_dir, "[1-9]*_*_*_*")
