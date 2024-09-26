bcec98fda552c2a98ebfcecf409d475d66bd3717
kedhammar <alfred.kedhammar@scilifelab.se>
2024-09-26 15:29:41 +0200

    build config from tmp dir and update tests to pass

--- tests/element/test_Aviti_Runs.py
+++ tests/element/test_Aviti_Runs.py
@@ -4,7 +4,7 @@ from unittest.mock import patch
 import pytest
 
 from taca.element import Aviti_Runs as to_test
-from tests.element.test_Element_Runs import CONFIG, create_element_run_dir
+from tests.element.test_Element_Runs import create_element_run_dir, get_config
 
 
 class TestAviti_Run:
@@ -16,6 +16,6 @@ class TestAviti_Run:
         mock_db = patch("taca.element.Element_Runs.ElementRunsConnection")
         mock_db.start()
 
-        run = to_test.Aviti_Run(run_dir, CONFIG)
+        run = to_test.Aviti_Run(run_dir, get_config(tmp))
         assert run.run_dir == run_dir
         assert run.sequencer_type == "Aviti"
