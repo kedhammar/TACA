120f9475f4bd3364e7e22a9b2948442283815d19
kedhammar <alfred.kedhammar@scilifelab.se>
2024-10-11 14:54:43 +0200

    mock subprocess, use default kwargs

--- tests/analysis/test_analysis_element.py
+++ tests/analysis/test_analysis_element.py
@@ -1,12 +1,9 @@
 from tempfile import TemporaryDirectory
 from unittest.mock import patch
 
-import pytest
-
 from tests.element.test_Element_Runs import create_element_run_dir, get_config
 
 
-@pytest.mark.skip(reason="Not implemented yet")
 def test_run_preprocessing(create_dirs):
     tmp: TemporaryDirectory = create_dirs
 
@@ -19,17 +16,14 @@ def test_run_preprocessing(create_dirs):
     mock_db = patch("taca.element.Element_Runs.ElementRunsConnection")
     mock_db.start()
 
+    # Mock subprocess
+    mock_subprocess = patch("subprocess.Popen")
+    mock_subprocess.start()
+
+    # Create run dir and associated LIMS manifest
+    run_dir = create_element_run_dir(tmp=tmp)
+
     # Import module to test
     from taca.analysis import analysis_element as to_test
-
-    run_dir = create_element_run_dir(
-        tmp=tmp,
-        nosync=False,
-        run_finished=False,
-        sync_finished=False,
-        demux_dir=False,
-        demux_done=False,
-        outcome_completed=False,
-    )
 
     to_test.run_preprocessing(run_dir)
