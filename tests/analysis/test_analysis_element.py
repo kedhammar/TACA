221a57a29eba4a0a9521c4feda297c60262bcd0c
kedhammar <alfred.kedhammar@scilifelab.se>
2024-10-24 13:20:49 +0200

    last commit worked, try removing debug statement

--- tests/analysis/test_analysis_element.py
+++ tests/analysis/test_analysis_element.py
@@ -190,9 +190,6 @@ def test_process_on_finished_run(aviti_fixture):
         ]
     )
 
-    for call in mocks["mock_popen"].call_args_list:
-        print(call)
-
     assert any(
         expected_cmd in call.args[0] for call in mocks["mock_popen"].call_args_list
     ), f"Expected command '{expected_cmd}' not found in any Popen calls."
